03278c6775966b44b8db3d79d7b6e39a560b6eae
Premith <premith@gmail.com>
2017-01-27 14:32:50 +0530

    Code cleanup

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
 var nodemailer = require('nodemailer');
 
-module.exports=function(options) {
+module.exports = function (options) {
 
-	 var transporter = nodemailer.createTransport({
+    var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
             user: options.email, // Your email id
             pass: options.password // Your password }
-   		 });
-	
-	}
+        }
+    });
+
+    var sendMail = function (mail) {
+
+        return new Promise(function (resolve, reject) {
+
+            var mailOptions = {
+                from: options.from, // sender address
+                to: [mail.to], // list of receivers
+                subject: mail.subject, // Subject line
+                text: mail.text //, // plaintext body
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                } else {
+                    console.log('Message sent: ' + info.response);
+                    resolve(info);
+                };
+            });
 
 
-   var ses = new AmazonSES(
-      options.accessKeyId,
-      options.secretAccessKey,
-      options.region
-   );
-
-   var sendMail=function(mail){
-
-      return new Promise(function(resolve, reject){
-
- 		var mailOptions = {
-    		from: options.from, // sender address
-   			to: [mail.to], // list of receivers
-    		subject: mail.subject, // Subject line
-    		text: mail.text //, // plaintext body
-		};
-
-		transporter.sendMail(mailOptions, function(error, info){
-    		if(error){
-        		console.log(error);
-                reject(error);
-    		}else{
-        		console.log('Message sent: ' + info.response);
-        		resolve(info);
-    		};
-		});
+        });
+    };
 
 
-         });
-      });
-  
-
-   return {
-      sendMail: sendMail
-   }
-};
+    return {
+        sendMail: sendMail
+    }
 };
 
